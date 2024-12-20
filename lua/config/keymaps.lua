f9b525a456d35f2340d08d812316cecac2cda775
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2024-12-20 17:41:46 +0100

    feat(lsp): add keymap to toggle BasedPyright settings
    - Added a keymap in keymaps.lua to toggle BasedPyright settings
    - Created lsp_utils.lua with a function to toggle typeCheckingMode and
    inlay hints for BasedPyright
    - Restart the LSP client to apply the new settings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local lsp_utils = require("utils.lsp_utils")
+
+-- stylua: ignore start
+
+-- Add a keymap for toggling BasedPyright settings
+vim.keymap.set("n", "<leader>ut", function() lsp_utils.toggle_basedpyright_settings() end, { desc = "Toggle BasedPyright Settings" })
+
+-- stylua: ignore end
---
+++ lua/utils/lsp_utils.lua
@@ -0,0 +1,38 @@
+local M = {}
+
+--- Toggle BasedPyright typeCheckingMode and inlay hints
+function M.toggle_basedpyright_settings()
+  -- Get the LSP client for basedpyright
+  local client = vim.lsp.get_clients({ name = "basedpyright" })[1]
+  if not client then
+    vim.notify("BasedPyright LSP is not active", vim.log.levels.WARN)
+    return
+  end
+
+  -- Toggle the typeCheckingMode
+  local analysis = client.config.settings.basedpyright.analysis
+  if analysis.typeCheckingMode == "basic" then
+    analysis.typeCheckingMode = "recommended"
+  else
+    analysis.typeCheckingMode = "basic"
+  end
+
+  -- Toggle the inlayHints settings
+  local hints = analysis.inlayHints
+  hints.variableTypes = not hints.variableTypes
+  hints.functionReturnTypes = not hints.functionReturnTypes
+
+  -- Restart the LSP to apply changes
+  vim.lsp.stop_client(client.id)
+  vim.defer_fn(function()
+    vim.cmd("LspStart basedpyright")
+    vim.notify(
+      "BasedPyright restarted with typeCheckingMode: "
+        .. analysis.typeCheckingMode
+        .. "\nInlay Hints: "
+        .. (hints.variableTypes and "enabled" or "disabled")
+    )
+  end, 100)
+end
+
+return M
