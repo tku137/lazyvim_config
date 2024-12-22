b98cc768204567a6f438599c450ef11d82dfafa4
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2024-12-22 12:37:01 +0100

    feat(colorscheme): use LazyVim catppuccin config
    - Added default colorschemes to the install list in lazy.lua
    - Updated colorscheme.lua to use use LazyVim's config instead of copying
    custom options

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,9 +17,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {
-      colorscheme = "catppuccin",
-    } },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
     { import = "plugins" },
     -- import any extras modules here
@@ -41,7 +39,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "catppuccin" } },
+  install = { colorscheme = { "catppuccin", "tokyonight", "habamax" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
