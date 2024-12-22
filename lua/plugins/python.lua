4f6b8646215e622d91e0a5171e5314be4ab3c3be
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2024-12-22 21:42:21 +0100

    feat(python): add nvim-python-repl plugin with key mappings
    - Added nvim-python-repl plugin with dependencies on nvim-treesitter
    - Configured key mappings for sending code to REPL:
      - <Leader>crr: Send semantic unit to REPL
      - <Leader>crb: Send entire buffer to REPL
      - <Leader>crE: Toggle automatic execution
      - <Leader>crV: Toggle vertical/horizontal split
      - <Leader>r: Send visual selection to REPL
    - Configured REPL settings for Python, Lua, and Scala

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -1,3 +1,5 @@
+local prefix = "<Leader>cr"
+
 return {
   {
     "neovim/nvim-lspconfig",
@@ -27,6 +29,84 @@ return {
           },
         },
       }
+    end,
+  },
+  {
+    "geg2102/nvim-python-repl",
+    dependencies = {
+      "nvim-treesitter",
+    },
+    keys = {
+      -- Normal mode keymaps
+      {
+        prefix .. "r",
+        function()
+          require("nvim-python-repl").send_statement_definition()
+        end,
+        desc = "Send semantic unit to REPL",
+        mode = "n",
+      },
+      {
+        "<Leader>r",
+        function()
+          require("nvim-python-repl").send_statement_definition()
+        end,
+        desc = "Send semantic unit to REPL",
+        mode = "n",
+      },
+      {
+        prefix .. "b",
+        function()
+          require("nvim-python-repl").send_buffer_to_repl()
+        end,
+        desc = "Send entire buffer to REPL",
+        mode = "n",
+      },
+      {
+        prefix .. "E",
+        function()
+          require("nvim-python-repl").toggle_execute()
+          vim.notify(
+            "Automatic REPL execution "
+              .. (require("nvim-python-repl.config").defaults["execute_on_send"] and "Enabled" or "Disabled")
+          )
+        end,
+        desc = "Toggle automatic execution",
+        mode = "n",
+      },
+      {
+        prefix .. "V",
+        function()
+          require("nvim-python-repl").toggle_vertical()
+          vim.notify(
+            "REPL split set to "
+              .. (require("nvim-python-repl.config").defaults["vsplit"] and "Vertical" or "Horizontal")
+          )
+        end,
+        desc = "Toggle vertical/horizontal split",
+        mode = "n",
+      },
+      -- Visual mode keymap
+      {
+        "<Leader>r",
+        function()
+          require("nvim-python-repl").send_visual_to_repl()
+        end,
+        desc = "Send visual selection to REPL",
+        mode = "v",
+      },
+    },
+    ft = { "python", "lua", "scala" },
+    config = function()
+      require("nvim-python-repl").setup({
+        execute_on_send = true,
+        vsplit = true,
+        spawn_command = {
+          python = "ipython",
+          scala = "sbt console",
+          lua = "ilua",
+        },
+      })
     end,
   },
 }
