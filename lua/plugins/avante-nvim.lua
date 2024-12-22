30b9cf2bc72abf3fade63da1836fac643073d5b2
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2024-12-22 21:45:33 +0100

    feat(AI): add avante.nvim
    - Configured key mappings for actions like ask, edit, refresh, focus,
    toggle, diff, and files
    - Set provider and auto_suggestions_provider to "copilot"
    - Included dependencies: plenary.nvim, nui.nvim, img-clip.nvim,
    render-markdown.nvim

---
+++ lua/plugins/avante-nvim.lua
@@ -0,0 +1,73 @@
+local prefix = "<Leader>a"
+return {
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false, -- set this if you want to always pull the latest change
+    opts = {
+      -- add any opts here
+      mappings = {
+        ask = prefix .. "<CR>",
+        edit = prefix .. "e",
+        refresh = prefix .. "r",
+        focus = prefix .. "f",
+        toggle = {
+          default = prefix .. "t",
+          debug = prefix .. "d",
+          hint = prefix .. "h",
+          suggestion = prefix .. "s",
+          repomap = prefix .. "R",
+        },
+        diff = {
+          next = "]c",
+          prev = "[c",
+        },
+        files = {
+          add_current = prefix .. ".",
+        },
+      },
+      provider = "copilot",
+      auto_suggestions_provider = "copilot",
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    -- dynamically build it, taken from astronvim
+    build = vim.fn.has("win32") == 1 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+      or "make",
+    dependencies = {
+      -- "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = function(_, opts)
+          opts.file_types = opts.file_types or {}
+          vim.list_extend(opts.file_types, { "Avante" })
+        end,
+        ft = function(ft_list)
+          ft_list = ft_list or {}
+          vim.list_extend(ft_list, { "markdown", "Avante" })
+          return ft_list
+        end,
+      },
+    },
+  },
+}
