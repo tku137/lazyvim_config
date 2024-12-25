182120fa4023a804d36a6747c98e2bb558181d78
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2024-12-25 19:59:35 +0100

    feat(breakpoints): add persistent-breakpoints-nvim plugin
    - Added `persistent-breakpoints-nvim` plugin to manage breakpoints.
    - Configured to load breakpoints on `BufReadPost` event.
    - Added key mappings for toggling, clearing, and setting conditional
      breakpoints:
      - `<Leader>db` to toggle breakpoint
      - `<Leader>dx` to clear all breakpoints
      - `<Leader>dB` to set conditional breakpoint

---
+++ lua/plugins/persistent-breakpoints-nvim.lua
@@ -0,0 +1,38 @@
+-- stylua: ignore
+-- if true then return {} end
+
+return {
+  "Weissle/persistent-breakpoints.nvim",
+  event = "BufReadPost",
+  opts = function()
+    require("persistent-breakpoints").setup({
+      load_breakpoints_event = { "BufReadPost" },
+    })
+  end,
+  keys = {
+    {
+      "<Leader>db",
+      function()
+        require("persistent-breakpoints.api").toggle_breakpoint()
+      end,
+      { silent = true },
+      desc = "Toggle Breakpoint",
+    },
+    {
+      "<Leader>dx",
+      function()
+        require("persistent-breakpoints.api").clear_all_breakpoints()
+      end,
+      { silent = true },
+      desc = "Clear Breakpoints",
+    },
+    {
+      "<Leader>dB",
+      function()
+        require("persistent-breakpoints.api").set_conditional_breakpoint()
+      end,
+      { silent = true },
+      desc = "Conditional Breakpoint",
+    },
+  },
+}
