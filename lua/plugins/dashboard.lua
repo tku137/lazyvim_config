83ecaccb66c6a9df7698723f21277273dcba4c09
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2024-12-25 21:14:10 +0100

    feat(dashboard): add snacks.nvim plugin configuration
    Added configuration for the snacks.nvim plugin to the dashboard. This includes
    sections for header, keymaps, recent files, and git status. The git status
    section is enabled only if a git root is detected and runs the command `git
    status --short --branch --renames`.

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,31 @@
+-- stylua: ignore
+-- if true then return {} end
+
+return {
+  "folke/snacks.nvim",
+  opts = {
+    dashboard = {
+      sections = {
+        { section = "header" },
+        -- { section = "keys", gap = 0, padding = 1 },
+        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+        { icon = " ", title = "Recent Files", section = "recent_files", limit = 5, cwd = true, indent = 2, padding = 1 },
+        -- { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 2 },
+        {
+          icon = " ",
+          title = "Git Status",
+          section = "terminal",
+          enabled = function()
+            return Snacks.git.get_root() ~= nil
+          end,
+          cmd = "git status --short --branch --renames",
+          height = 5,
+          padding = 1,
+          ttl = 5 * 60,
+          indent = 2,
+        },
+        { section = "startup" },
+      },
+    },
+  },
+}
