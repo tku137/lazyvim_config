b7c0afac5ad7d3a1f7918f890452d7e432b4bdad
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2025-01-08 19:41:39 +0100

    feat(keymaps): add DAP function keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,4 +9,12 @@ local lsp_utils = require("utils.lsp_utils")
 -- Add a keymap for toggling BasedPyright settings
 vim.keymap.set("n", "<leader>ut", function() lsp_utils.toggle_basedpyright_settings() end, { desc = "Toggle BasedPyright Settings" })
 
+-- Add some DAP mappings
+vim.keymap.set("n", "<F5>", function() require("dap").continue() end, { desc = "Debugger: Start" })
+vim.keymap.set("n", "<F6>", function() require("dap").pause() end, { desc = "Debugger: Pause" })
+vim.keymap.set("n", "<F9>", function() require("dap").toggle_breakpoint() end, { desc = "Debugger: Toggle Breakpoint" })
+vim.keymap.set("n", "<F10>", function() require("dap").step_over() end, { desc = "Debugger: Step Over" })
+vim.keymap.set("n", "<F11>", function() require("dap").step_into() end, { desc = "Debugger: Step Into" })
+vim.keymap.set("n", "<F17>", function() require("dap").terminate() end, { desc = "Debugger: Terminate" }) -- Shift+F5
+vim.keymap.set("n", "<F23>", function() require("dap").step_out() end, { desc = "Debugger: Step Out" }) -- Shift+F11
 -- stylua: ignore end
