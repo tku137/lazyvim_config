25b64fd73da56f125b43f803f2757e0f094d1368
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2025-01-18 16:46:36 +0100

    fix(ai): preserve default lazyvim settings
    Using the old method, the default options from LazyVim were overridden.
    This makes sure we use whatever was set by LazyVim and just change the
    model.

--- after/plugin/copilot_chat.lua
+++ after/plugin/copilot_chat.lua
@@ -2,6 +2,8 @@
 -- opts table in lua/plugins/ai.lua did not change the
 -- model used when the plugin is lazy-loaded.
 -- Using this, we just manually set the model.
-require("CopilotChat").setup({
+local current_config = require("CopilotChat").config
+local new_config = vim.tbl_deep_extend("force", current_config or {}, {
   model = "claude-3.5-sonnet",
 })
+require("CopilotChat").setup(new_config)
