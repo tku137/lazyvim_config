3412b0b81a61cddf2364ea3e01a03e073c758f38
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2025-02-14 10:22:33 +0100

    refactor: remove sql extra

--- lua/extras/langs.lua
+++ lua/extras/langs.lua
@@ -2,7 +2,7 @@ return {
   -- Language support extras
   { import = "lazyvim.plugins.extras.lang.python" },
   { import = "lazyvim.plugins.extras.lang.clangd" },
-  { import = "lazyvim.plugins.extras.lang.sql" },
+  -- { import = "lazyvim.plugins.extras.lang.sql" },
   { import = "lazyvim.plugins.extras.lang.docker" },
   { import = "lazyvim.plugins.extras.lang.git" },
   { import = "lazyvim.plugins.extras.lang.json" },
